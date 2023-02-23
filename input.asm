4ecca5fdd2708016b38cfd1d4776dd38a319574c
irfan378 <irfandominate@gmail.com>
2023-02-23 19:35:51 +0530

    math

--- input.asm
+++ input.asm
@@ -1,7 +1,7 @@
 global _start
 section .text
 _start:
-    mov rax,1
+    mov rax,1 
     mov rdi,1
     mov rsi,hello
     mov rdx,hello_length
---
+++ math.asm
@@ -0,0 +1,20 @@
+global main
+section .text
+main:
+    mov rax,2
+    add rax,3
+
+    mov rax,2
+    sub rax,2
+
+    mov rax,6
+    imul rax,rax,2 
+
+    mov rdx,0
+    mov rax,100
+    mov rbx,2
+    idiv rbx
+_exit:
+    mov rax,60
+    mov rdi,0
+    syscall
